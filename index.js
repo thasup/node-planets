3d6b0fd82c1c2a4e06703df4d30893c06fc1d60f
thasup <thanachonfirst@hotmail.com>
2023-04-16 02:15:53 +0700

    Add constants

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const { parse } = require('csv-parse');
 const fs = require('fs');
 
 const habitablePlanets = [];
+const MIN_INSOLATION_FLUX = 0.36;
+const MAX_INSOLATION_FLUX = 1.11;
+const PLANETARY_RADIUS = 1.6;
 
 function isHabitablePlanet(planet) {
   return planet['koi_disposition'] === 'CONFIRMED'
-    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
-    && planet['koi_prad'] < 1.6;
+    && planet['koi_insol'] > MIN_INSOLATION_FLUX && planet['koi_insol'] < MAX_INSOLATION_FLUX
+    && planet['koi_prad'] < PLANETARY_RADIUS;
 }
 
 fs.createReadStream('kepler_data.csv')
@@ -23,8 +26,9 @@ fs.createReadStream('kepler_data.csv')
     console.log(err);
   })
   .on('end', () => {
-    console.log(habitablePlanets.map((planet) => {
+    const planetNames = habitablePlanets.map((planet) => {
       return planet['kepler_name'];
-    }));
+    })
+    console.log(planetNames);
     console.log(`${habitablePlanets.length} habitable planets found!`);
   });
